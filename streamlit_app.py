bc33dbde5fd738f9c6b730914e18000f6b6091ae
L1ght94 <108329446+L1ght94@users.noreply.github.com>
2024-10-16 10:42:31 +0900

    Update streamlit_app.py
    Show fruit data for each ingredient chose

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,11 @@ ingredients_list = st.multiselect(
 
 if ingredients_list:
     ingredients_string = ''
+
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     # st.write(ingredients_string)
 
@@ -41,6 +44,3 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success(name_on_order + ', your Smoothie is ordered!', icon="✅")
 
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
